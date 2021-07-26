a26f1fdacacc2bb37f459a1b064c076426549b55
Sudarsan1234 <57763360+Sudarsan1234@users.noreply.github.com>
2021-07-26 17:42:01 +0530

    Add files via upload

---
+++ src/com/onlineexam/main/AddSubject.java
@@ -0,0 +1,41 @@
+package com.onlineexam.main;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.onlineexam.Dao.SubjectDao;
+import com.onlineexam.bean.Subjects;
+
+/**
+ * Servlet implementation class AddSubject
+ */
+@WebServlet("/AddSubject")
+public class AddSubject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String sname = request.getParameter("sname");
+		Subjects sub = new Subjects(sname);
+		SubjectDao obj = new SubjectDao();
+		obj.insertSubject(sub);
+		RequestDispatcher rd = request.getRequestDispatcher("subject.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/onlineexam/main/DeleteSubject.java
@@ -0,0 +1,44 @@
+package com.onlineexam.main;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.onlineexam.Dao.SubjectDao;
+
+/**
+ * Servlet implementation class DeleteStudent
+ */
+@WebServlet("/deleteSubject")
+public class DeleteSubject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("sid"));
+		SubjectDao obj = new SubjectDao();
+		try {
+			obj.deleteSubject(id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("subject.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
